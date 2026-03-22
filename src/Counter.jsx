d95ea7a649128aac7ed859ccfaa2db765b94c510
Pon-Ponovskiy <celoveka269@gmail.com>
2026-03-22 19:42:02 +0300

    22/03/2026

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,19 +2,7 @@ import React, { useState } from "react";
 import './Counter.css';
 
 function Counter(){
-    let plus = document.querySelector('.plus')
-
     const [count, setCount] = useState(0)
-
-    function onClick(){
-        plus = document.addEventListener('click', ()=>{
-            useState += 1;
-        })
-    }
-    onClick()
-    console.log(count)
-
-
 
     
 
@@ -22,9 +10,13 @@ function Counter(){
         <div class='Counter'>
             <div>
             <h2>Счетчик:</h2>
-            <h1>0</h1>
-            <button class='minus'>-Минус</button>
-            <button class='plus'>Плюс+  </button>
+            <h1>{count}</h1>
+            <button onClick={()=>{
+                setCount(count-1)
+            }} class='minus'>-Минус</button>
+            <button onClick={()=>{
+                setCount(count+1)
+            }} class='plus'>Плюс+ </button>
         </div>
         </div>
     )
